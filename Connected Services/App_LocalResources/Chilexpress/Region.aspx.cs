45acb195a78de86dd29c0b609caf09f32eae424d
Felipe <felipeinostrozadiaz@gmail.com>
2021-03-16 00:14:42 -0300

    integracion chilexpress

---
+++ Connected Services/App_LocalResources/Chilexpress/Region.aspx.cs
@@ -0,0 +1,17 @@
+﻿namespace UMTransporte.App_LocalResources.Chilexpress.Region
+{
+    public class Region
+    {
+        public string RegionId { get; set; }
+        public string RegionName { get; set; }
+        public int IneRegionCode { get; set; }
+    }
+
+    public class Root
+    {
+        public System.Collections.Generic.List<Region> Regions { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+        public object Errors { get; set; }
+    }
+}
